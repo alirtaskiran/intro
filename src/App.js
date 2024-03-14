1a4a784a90c9f7c7dd91f41be9d9583ffe80ba6b
alirtaskiran <alirtaskiran@gmail.com>
2024-03-14 23:05:07 +0300

    Revert "clean project and js file added"
    This reverts commit 8a8400f4e3e93e341616928706bedf84fa581d0e.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Navi from "./Navi";
-import CategoryList from "./CategoryList.js";
-import ProductList from "./ProductList.js";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <Navi />
-      <CategoryList />
-      <ProductList />
-      <h2>Hello From React!</h2>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
