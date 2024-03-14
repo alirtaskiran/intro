2df08b7dd67f76903c9d93f1d3da2a1af6e88e85
alirtaskiran <alirtaskiran@gmail.com>
2024-03-15 00:17:32 +0300

    i bootstrap and use reactstrap components

---
+++ src/Navi.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+  render() {
+    return (
+      <div>
+        <h2>NaviBar Component</h2>
+      </div>
+    )
+  }
+}
