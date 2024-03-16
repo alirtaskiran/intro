03419bb80d972cd08fb8f0bcc2db22e573386b9d
alirtaskiran <alirtaskiran@gmail.com>
2024-03-16 23:17:57 +0300

    convert class component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
 import { Container, Row, Col } from "react-bootstrap";
 
-function App() {
-  let productInfo = { title: "Product List", baskaBirSey: "Baska bir sey" };
-  let categoryInfo = { title: "Category List" };
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi></Navi>
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo} />
-          </Col>
-          <Col xs="9">
-            <ProductList info={productInfo} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+export default class App extends Component {
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
+  render() {
+    let productInfo = { title: "Product List", baskaBirSey: "Baska bir sey" };
+    let categoryInfo = { title: "Category List" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi></Navi>
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
